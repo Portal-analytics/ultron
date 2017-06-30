d1d42e53dfd2ba265c746f8cb09dabc78e0624f7
Kavya Ravikanti <Kavya@kavyas-mbp.lan>
2017-06-30 15:46:42 -0400

    styling

--- react-vr/index.vr.js
+++ react-vr/index.vr.js
@@ -1,25 +1,76 @@
 import React from "react";
-import { AppRegistry, asset, Pano, Text, View } from "react-vr";
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+  VrButton,
+  Image
+} from "react-vr";
 
 export default class WelcomeToVR extends React.Component {
+  state = {
+    source: "chess-world.jpg"
+  };
+
+  onViewClicked = () => {
+    this.setState = {
+      source: "sample.jpg"
+    };
+    console.log("click");
+  };
+
   render() {
     return (
       <View>
-        <Pano source={asset("chess-world.jpg")} />
+        <Pano source={asset(this.state.source)} />
         <Text
           style={{
-            backgroundColor: "#777879",
+            backgroundColor: "darkblue",
             fontSize: 0.8,
-            fontWeight: "400",
+            fontWeight: "200",
             layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
             textAlign: "center",
             textAlignVertical: "center",
-            transform: [{ translate: [0, 0, -3] }]
+            transform: [{ translate: [0, 0.5, -7] }]
           }}
         >
-          hello
+          Welcome to
+        </Text>
+        <Text
+          style={{
+            backgroundColor: "darkblue",
+            fontSize: 0.8,
+            fontWeight: "200",
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: "center",
+            textAlignVertical: "center",
+            transform: [{ translate: [0, 0.5, -7] }]
+          }}
+        >
+          Ultron
+        </Text>
+        <Text
+          style={{
+            backgroundColor: "black",
+            width: 2,
+            fontSize: 0.2,
+            fontWeight: "100",
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: "center",
+            textAlignVertical: "center",
+            transform: [{ translate: [0, 0, -5] }]
+          }}
+          onPress={() => console.log("press")}
+        >
+          click here to enter
         </Text>
       </View>
     );
